a313c27d43385b675dce890f368e9fc3e3000044
Toby Dylan Hocking <tdhock5@gmail.com>
2014-05-27 14:59:19 -0400

    data structures

--- src/matched_diff.h
+++ src/matched_diff.h
@@ -1,1 +1,26 @@
+#include <list>
+#include <map>
+
+class Match;
+typedef std::multimap< int, Match* > SortedMatches;
+typedef std::pair< int, Match* > JaccardMatch;
+
+class Peak;
+typedef std::list< Peak* > Peaks;
+
+class Match {
+public:
+    Peaks::iterator one;
+    Peaks::iterator two;
+}
+
+class Peak {
+public:
+    int chromStart;
+    int chromEnd;
+    int track;
+    std::list< SortedMatches::iterator > matches;
+}
+
 int matched_diff(int*, int*, int*, int, int*, int*);
+
