392cf03d6674c3b80a91504ec9058f7bcf6a19fc
Toby Dylan Hocking <tdhock5@gmail.com>
2014-05-27 17:20:43 -0400

    begin algos

--- src/matched_diff.cpp
+++ src/matched_diff.cpp
@@ -1,3 +1,5 @@
+// #include <cstdio>
+// #include <limits>
 #include "matched_diff.h"
 
 int
@@ -8,5 +10,45 @@ matched_diff
  int peak_count,
  int *diff_chromStart,
  int *diff_chromEnd){
-  return ERROR_UNRECOGNIZED_PEAK_TRACK;
+  int peak_i = 0, diff_i = 0, status;
+  PeakMatches peak_matches;
+  Peak *peak;
+  while(peak_i < peak_count) {
+    peak_matches.init();
+    bool next_overlaps = 
+      peak_i < peak_count && 
+      peak_chromStart[peak_i] < peak_matches.get_chromEnd();
+    // Add peaks and matches until there are no more overlapping
+    // peaks.
+    while(peak_matches.get_chromEnd() == 0 || next_overlaps){
+      peak = new Peak(peak_chromStart[peak_i], 
+		      peak_chromEnd[peak_i],
+		      peak_track[peak_i]);
+      status = peak_matches.add_peak(peak);
+      peak_i++;
+      if(status < 0){
+	return status;
+      }
+    }
+    peak_matches.remove_matching();
+    while(peak_matches.has_peaks()){
+      peak = peak_matches.pop_peak();
+      diff_chromStart[diff_i] = peak->get_chromStart();
+      diff_chromEnd[diff_i] = peak->get_chromEnd();
+      delete peak;
+      diff_i++;
+    }
+  }
+  
+  // 249250621 for hg19:chr1.
+  // 248956422 for hg38:chr1.
+  // 2147483647 returned by std::numeric_limits<int>::max().
+  // 2000000000 == 2e9L in R (integer implemented as long).
+  // So this code should work fine.
+
+  // int imin = std::numeric_limits<int>::min(); // minimum value
+  // int imax = std::numeric_limits<int>::max();
+  // printf("%d %d\n", imin, imax);
+   
+    return diff_i;
 }
--- src/matched_diff.h
+++ src/matched_diff.h
@@ -2,26 +2,95 @@
 #include <map>
 
 class Match;
-typedef std::multimap< int, Match* > SortedMatches;
-typedef std::pair< int, Match* > JaccardMatch;
+typedef std::multimap< double, Match* > SortedMatches;
+typedef std::pair< double, Match* > JaccardMatch;
 
 class Peak;
-typedef std::list< Peak* > Peaks;
+typedef std::list< Peak* > PeakList;
 
 class Match {
+    PeakList::iterator one;
+    PeakList::iterator two;
 public:
-    Peaks::iterator one;
-    Peaks::iterator two;
 };
 
 class Peak {
-public:
     int chromStart;
     int chromEnd;
     int track;
     std::list< SortedMatches::iterator > matches;
+public:
+    int get_chromStart(){
+	return this->chromStart;
+    }
+    int get_chromEnd(){
+	return this->chromEnd;
+    }
+    int get_track(){
+	return this->track;
+    }
+    Peak(int chromStart, int chromEnd, int track){
+	this->chromStart = chromStart;
+	this->chromEnd = chromEnd;
+	this->track = track;
+    }
 };
 
 int matched_diff(int*, int*, int*, int, int*, int*);
 
 #define ERROR_UNRECOGNIZED_PEAK_TRACK -1
+
+class PeakMatches {
+    SortedMatches sorted_matches;
+    PeakList peak_list;
+    int peak_list_chromEnd;
+    PeakList::iterator last_one_it, last_two_it;
+public:
+    void init(){
+	last_one_it = last_two_it = peak_list.end(); // NULL.
+	int peak_list_chromEnd = 0;
+    }
+    int get_chromEnd(){
+	return this->peak_list_chromEnd;
+    }
+    int add_peak(Peak *peak){
+	PeakList::iterator peak_it;
+	this->peak_list.push_front(peak);
+	peak_it = this->peak_list.begin();
+	if(peak_list_chromEnd < peak->get_chromEnd()){
+	    peak_list_chromEnd = peak->get_chromEnd();
+	}
+	switch(peak->get_track()) {
+	case 1:
+	    add_match(last_two_it, peak_it);
+	    last_one_it = peak_it;
+	    break;
+	case 2:
+	    add_match(last_one_it, peak_it);
+	    last_two_it = peak_it;
+	    break;
+	default:
+	    return ERROR_UNRECOGNIZED_PEAK_TRACK;
+	}
+	return 0;
+    }
+    void add_match(PeakList::iterator last_it, PeakList::iterator peak_it){
+	if(last_it == peak_list.end()){
+	}
+    }
+    void remove_matching(){
+	while(sorted_matches.size()){
+	}
+    }
+    bool has_peaks(){
+	return !peak_list.empty();
+    }
+    Peak* pop_peak(){
+	PeakList::iterator peak_it;
+	Peak *peak;
+	peak_it = peak_list.begin();
+	peak = *peak_it;
+	peak_list.erase(peak_it);
+	return peak;
+    }
+};
